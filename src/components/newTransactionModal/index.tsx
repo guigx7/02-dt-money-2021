e9b56d1d75a43732e67e1ccdc2fba0dd560964b1
Guilherme Tavares <guigo211103@gmail.com>
2022-03-25 16:36:40 -0300

    C2 - Aula 4-9
    Transação inserida na API (miragejs)

--- src/components/newTransactionModal/index.tsx
+++ src/components/newTransactionModal/index.tsx
@@ -3,6 +3,7 @@ import Modal from "react-modal";
 import closeImg from "../../assets/close.svg";
 import incomeImg from "../../assets/income.svg";
 import outcomeImg from "../../assets/outcome.svg";
+import { api } from "../../services/api";
 import { Container, RadioBox, TranscationTypeContainer } from "./styles";
 
 interface NewTransactionModalProps {
@@ -18,7 +19,9 @@ export function NewTransactionModal({isOpen, onRequestClose,}: NewTransactionMod
 
   function handleCreateNewTransaction(event: FormEvent){
     event.preventDefault();
-    console.log({title, value, category, type})
+    const data = ({title, value, category, type})
+    console.log(data)
+    api.post('/transactions', data)
   }
 
   return (
