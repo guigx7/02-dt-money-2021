9c6ee84f5c12ca012103215fe7c9121c64d39eea
Guilherme Tavares <guigo211103@gmail.com>
2022-03-25 16:54:48 -0300

    C2 - Aula 4-10
    Tabela retornando API, listando as transações

--- src/index.tsx
+++ src/index.tsx
@@ -1,33 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { createServer, Model } from 'miragejs';
-import { App } from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createServer, Model } from "miragejs";
+import { App } from "./App";
 
 createServer({
   models: {
     transaction: Model,
   },
 
-  routes(){
-    this.namespace = 'api';
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Test",
+          type: "deposit",
+          category: "test",
+          amount: 1000,
+          createdAt: new Date("2022-03-24"),
+        },
+        {
+          id: 2,
+          title: "Test2",
+          type: "deposit",
+          category: "test2",
+          amount: 3000,
+          createdAt: new Date("2022-03-24"),
+        },
+        {
+          id: 3,
+          title: "Test3",
+          type: "withdraw",
+          category: "test3",
+          amount: 2500,
+          createdAt: new Date("2022-03-25"),
+        },
+      ],
+    });
+  },
 
-    this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
+  routes() {
+    this.namespace = "api";
 
-    this.post('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
+    this.get("/transactions", () => {
+      return this.schema.all("transaction");
+    });
 
-      return schema.create('transaction', data)
-    })
+    this.post("/transactions", (schema, request) => {
+      const data = JSON.parse(request.requestBody);
 
-  }
-})
+      return schema.create("transaction", data);
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
